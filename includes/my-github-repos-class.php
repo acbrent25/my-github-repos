64631ae2015e4e0c934892d31edd7dbe2143de18
acbrent25 <adamchampagne@gmail.com>
2019-04-18 06:02:54 -0400

    added api call

--- includes/my-github-repos-class.php
+++ includes/my-github-repos-class.php
@@ -13,6 +13,20 @@ class WP_My_Github_Repos extends WP_Widget{
 
     // Frontend Display
     public function widget($args, $instance){
+        // get values
+        $title = apply_filters('widget_title', $instance['title']);
+        $username = esc_attr($instance['username']);
+        $count = esc_attr($instance['count']);
+
+        echo $args['before_widget'];
+
+        if(!empty($title)){
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+
+        echo $this->showRepos($title, $username, $count);
+
+        echo $args['after_widget'];
 
     }
 
@@ -57,9 +71,38 @@ class WP_My_Github_Repos extends WP_Widget{
 
     // Update widget values
     public function update($new_instance, $old_instance) {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['username'] = (!empty($new_instance['username'])) ? strip_tags($new_instance['username']) : '';
+        $instance['count'] = (!empty($new_instance['count'])) ? strip_tags($new_instance['count']) : '';
+
+        return $instance;
 
     }
 
+    // Show Repos
+    public function showRepos($title, $username, $count){
+        $url = 'https://api.github.com/users/'.$username.'/repos?sort=create&per_page='.$count;
+        $options = array('http' => array('user_agent' => $_SERVER['HTTP_USER_AGENT']));
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+        $repos = json_decode($response);
+
+        // Build Output
+        $output = '<ul class="repos">';
+
+        foreach($repos as $repos){
+            $output .= '<li>
+                            <div class="repo-title">'.$repos->name.'</div>
+                            <div class="repo-desc">'.$repos->description.'</div>
+                            <a target="_blank"  href="'.$repos->html_url.'">View on Github</a>
+                        </li>';
+        }
+
+        $output .= '</ul>';
+
+        return $output;
+    }
 
 
 }
